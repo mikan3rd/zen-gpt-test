5054907e5b8bb580bedcea073d299643757da1bd
mikan3rd <hiroki.ota@zenkigen.jp>
2023-04-19 22:48:19 +0900

    chore: add langchain to package.json

---
+++ src/components/pages/test.tsx
@@ -0,0 +1,11 @@
+import { OpenAI } from "langchain/llms/openai";
+
+export const TestPage = () => {
+  return (
+    <>
+      <main>
+        Hello World2!!
+      </main>
+    </>
+  )
+}
---
+++ src/pages/test.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+import { TestPage } from '@/components/pages/test'
+
+ const Test = () => {
+  return (
+    <>
+      <Head>
+        <title>Zen GPT</title>
+      </Head>
+      <TestPage />
+    </>
+  )
+}
+
+
+export default Test
