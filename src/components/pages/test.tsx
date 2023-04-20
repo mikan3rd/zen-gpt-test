441529988e2e8ebc4ab82628b1bfa2e08fb62d25
mikan3rd <hiroki.ota@zenkigen.jp>
2023-04-20 14:17:12 +0900

    chore: add test data

--- src/components/pages/test.tsx
+++ src/components/pages/test.tsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { OpenAI } from "langchain/llms/openai";
 import { CallbackManager } from "langchain/callbacks";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage } from "langchain/schema";
+import { ConversationChain } from "langchain/chains";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+  MessagesPlaceholder,
+} from "langchain/prompts";
+import { BufferMemory } from "langchain/memory";
 
 import {
   Button,
@@ -15,6 +23,8 @@ import {
 } from "semantic-ui-react";
 import styles from "@/styles/Test.module.css";
 
+import { HumanTemplate } from "@/utlils/template";
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const model = new OpenAI({
   openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // 本番環境ではサーバー側で実行すること
@@ -23,14 +33,43 @@ const model = new OpenAI({
 
 const chat = new ChatOpenAI({
   openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // 本番環境ではサーバー側で実行すること
+  temperature: 0,
+  streaming: true,
 });
 
 export const TestPage = () => {
+  const chainRef = useRef<ConversationChain>();
+
   const [loading, setLoading] = useState<boolean>(false);
-  const [text, setText] = useState<string>(
-    "「猫は液体」と言われる理由について子供が興味を引くように説明してください"
-  );
+  const [text, setText] = useState<string>("あなたの名前を教えてください");
   const [answer, setAnswer] = useState<string>("");
+
+  useEffect(() => {
+    setLoading(true);
+
+    chat.callbackManager = CallbackManager.fromHandlers({
+      async handleLLMNewToken(token: string) {
+        setAnswer((prev) => prev + token);
+      },
+    });
+
+    console.log(HumanTemplate);
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+      SystemMessagePromptTemplate.fromTemplate(HumanTemplate),
+      new MessagesPlaceholder("history"),
+      HumanMessagePromptTemplate.fromTemplate("{input}"),
+    ]);
+
+    const chain = new ConversationChain({
+      memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+      prompt: chatPrompt,
+      llm: chat,
+    });
+
+    chainRef.current = chain;
+
+    setLoading(false);
+  }, []);
 
   const onClickButton = async () => {
     setLoading(true);
@@ -40,13 +79,9 @@ export const TestPage = () => {
     // console.log(res);
     // setAnswer(res.text);
 
-    chat.streaming = true;
-    chat.callbackManager = CallbackManager.fromHandlers({
-      async handleLLMNewToken(token: string) {
-        setAnswer((prev) => prev + token);
-      },
+    await chainRef?.current?.call({
+      input: text,
     });
-    await chat.call([new HumanChatMessage(text)]);
 
     setLoading(false);
   };
